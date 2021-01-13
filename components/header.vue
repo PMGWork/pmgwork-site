71319722137d52492263b233ad86a0f8643dfce1
ぴくせる <48606231+PMGWork@users.noreply.github.com>
2021-01-13 15:51:55 +0900

    update

--- components/header.vue
+++ components/header.vue
@@ -16,6 +16,13 @@
 <script>
 export default {
     mounted(){
+        //height
+        let vh = window.innerHeight * 0.01;
+        document.documentElement.style.setProperty('--vh', `${vh}px`);
+        window.addEventListener('resize', () => {
+            let vh = window.innerHeight * 0.01;
+            document.documentElement.style.setProperty('--vh', `${vh}px`);
+        });
         //lottie
         lottie.loadAnimation({
             container: document.getElementById('lottie-logo'),path: '/animation/logomotion.json',renderer: 'svg',loop: true,autoplay: true
--- pages/index.vue
+++ pages/index.vue
@@ -37,13 +37,6 @@ export default {
     mounted(){
         document.getElementById("lottie-logo").style.opacity = '0';
 
-        let vh = window.innerHeight * 0.01;
-        document.documentElement.style.setProperty('--vh', `${vh}px`);
-        window.addEventListener('resize', () => {
-            let vh = window.innerHeight * 0.01;
-            document.documentElement.style.setProperty('--vh', `${vh}px`);
-        });
-
         const scene = document.getElementById('scene');
         const parallaxInstance = new Parallax(scene);
 
