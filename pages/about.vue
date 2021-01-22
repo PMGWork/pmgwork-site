83ac8f47bcc1961b45aaf86b39a04f19cd424d22
ぴくせる <48606231+PMGWork@users.noreply.github.com>
2021-01-22 13:42:47 +0900

    update

--- pages/about.vue
+++ pages/about.vue
@@ -69,7 +69,7 @@
             </div>
         </div>
         <footer class="footer">
-            <p class="copyright">© 2020 Pixel</p>
+            <p class="copyright">© 2021 Pixel</p>
             <div class="footer-link">
                 <a href="https://twitter.com/pmgwork" target="_blank" rel="noopener noreferrer">
                     <img class="desvg" src="https://simpleicons.org/icons/twitter.svg">
