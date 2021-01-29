57d09df5eab31e3193aba7e69ed4c94fa188808d
ぴくせる <48606231+PMGWork@users.noreply.github.com>
2021-01-29 10:17:31 +0900

    update

--- pages/shorts.vue
+++ pages/shorts.vue
@@ -10,7 +10,15 @@
             <div class="shorts-article" v-for="short in shorts" :key="short.id">
                 <div class="shorts-image">
                     <a :href="short.link" target="_blank" rel="noopener noreferrer">
-                        <video v-if="short.video" :src="short.video.url" autoplay loop muted playsinline oncontextmenu="return false;" onselectstart="return false;" onmousedown="return false;"></video>
+                        <video v-if="short.video"
+                            :src="short.video.url"
+                            :height="540"
+                            autoplay loop muted playsinline
+                            oncontextmenu="return false;"
+                            onselectstart="return false;"
+                            onmousedown="return false;"
+                        >
+                        </video>
                         <picture v-if="short.image">
                             <source :srcset="short.image.webp" type="image/webp">
                             <img
